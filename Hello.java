dcbeb9a15aa6c9393f258324240574256c67391d
yujin <yuejin060818@gmail.com>
2024-03-06 08:38:10 +0900

    오류사항확인

--- Hello.java
+++ Hello.java
@@ -5,13 +5,14 @@ public class Hello {
  System.out.println("Hellotest");
  System.out.println("Hellotest");
  System.out.println("Hellotest yuejin test");
-<<<<<<< HEAD
-System.out.println("hello!");
-=======
- System.out.println("Hellotest dong test");
+*<<<<<<< HEAD
+*System.out.println("hello!");
+*=======
+* System.out.println("Hellotest dong test");
 
 >>>>>>> 6e4d6b99f8c5ca332897b9784d1bf57521b261b7
-This branch has conflicts that must be resolved -> 동영이형이 push하신거 pull 안하고 수정하니까 뜬 오류
-						   맞는지는 모르겠지만 수정사항 있으면 꼭 git pull origin main 하고 push해야할듯 ??
+This branch has conflicts that must be resolved 
+-> [pull origin main을 한 후에 다른 작업자가 저와 같은 라인을 수정하고, 그 수정한 내역을 master 와 합쳤을때(merge) 발생]
+   본인이 pull 안하고 수정사항도 확인 안하고 로컬에서 수정한다음 push하니까 발생했음.
     }
 }
