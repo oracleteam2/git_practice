eab7b5d41eb5cb53988f46f5cc925483a842dac4
yujin <yuejin060818@gmail.com>
2024-03-06 08:25:44 +0900

    오류사항 추가

--- Hello.java
+++ Hello.java
@@ -11,6 +11,7 @@ System.out.println("hello!");
  System.out.println("Hellotest dong test");
 
 >>>>>>> 6e4d6b99f8c5ca332897b9784d1bf57521b261b7
-This branch has conflicts that must be resolved
+This branch has conflicts that must be resolved -> 동영이형이 push하신거 pull 안하고 수정하니까 뜬 오류
+						   맞는지는 모르겠지만 수정사항 있으면 꼭 git pull origin main 하고 push해야할듯 ??
     }
 }
