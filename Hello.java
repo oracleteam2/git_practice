a296b61ce2d8bdf29f47f8ebcae0e72f17c11ed8
pendrake <kamelot225@gmail.com>
2024-04-02 17:31:22 +0900

    123

--- Hello.java
+++ Hello.java
@@ -5,21 +5,18 @@ public class Hello {
  System.out.println("Hellotest");
  System.out.println("Hellotest");
  System.out.println("Hellotest yuejin test");
-<<<<<<< HEAD
-*<<<<<<< HEAD
-*System.out.println("Hello!");
-*=======
-* System.out.println("hellotest dong 12312asdasdasd3");
+
+System.out.println("Hello!");
+
+System.out.println("hellotest dong 12312asdasdasd3");
 
 
-test develop
->>>>>>> 6e4d6b99f8c5ca332897b9784d1bf57521b261b7
-=======
+
 System.out.println("Hello!");
 System.out.println("hellotest dong 123123");
 tttttttttttttttttttttttttttt choeiaoienyaoien
 test
->>>>>>> 3ea9efeb1741e31f892efcdfd9e70526a9a640d6
+
 This branch has conflicts that must be resolved 
 -> [pull origin main을 한 후에 다른 작업자가 저와 같은 라인을 수정하고, 그 수정한 내역을 master 와 합쳤을때(merge) 발생]
    본인이 pull 안하고 수정사항도 확인 안하고 로컬에서 수정한다음 push하니까 발생했음.
